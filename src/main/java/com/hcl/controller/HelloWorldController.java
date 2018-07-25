390136bdd77519c1c4b75f9e0a1136b7bea755f3
kalra_m <kalra_m@ip-172-20-128-1.ec2.internal>
2018-07-25 09:43:34 +0000

    first commit

---
+++ src/main/java/com/hcl/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.hcl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class HelloWorldController {
+ 
+	@RequestMapping("/helloworld")
+	public ModelAndView hello() {
+ 
+		String helloWorldMessage = "Hello world from devsecops!";
+		return new ModelAndView("hello", "message", helloWorldMessage);
+	}
+}
